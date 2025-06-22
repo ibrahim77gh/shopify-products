16efb54a745279086f00acd2b9eb5c9b30282b08
ibrahim77gh <syedibrahim4091@gmail.com>
2025-06-22 16:01:43 +0500

    Added requirements.txt

--- product_management/settings.py
+++ product_management/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djoser',
     'rest_framework',
+    'corsheaders',
     'django_filters',
     'celery',
     'django_celery_results',
@@ -55,6 +57,7 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
